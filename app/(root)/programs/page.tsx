5c777870a4294c712bc1c8e93d36658ceabc0e93
Subrat Chowdhary <subratchowdhary@outlook.com>
2023-11-18 16:30:14 +0530

    Updated Additition part of crud operations

--- app/(root)/programs/page.tsx
+++ app/(root)/programs/page.tsx
@@ -1,9 +1,10 @@
 // ProgramsPage.tsx
-"use client"
+"use client";
 
 import React, { useState, useEffect } from 'react';
 import ProgramsTable from '@/components/ProgramsTable';
 import { ColumnDef } from '@tanstack/react-table';
+import Programs from '@/components/ui/crud-programs';
 
 export type Program = {
   id: number;
@@ -31,19 +32,27 @@ export const programColumns: ColumnDef<Program>[] = [
   {
     accessorKey: 'startDate',
     header: 'Start Date',
-    cell: ({ row }) => <div>{row.getValue('startDate')}</div>,
+    cell: ({ row }) => (
+    <div>
+      {new Intl.DateTimeFormat('en-US', { day: 'numeric', month: 'short', year: 'numeric' }).format(new Date(row.getValue('endDate')))}
+    </div>
+  ),
   },
   {
     accessorKey: 'endDate',
     header: 'End Date',
-    cell: ({ row }) => <div>{row.getValue('endDate')}</div>,
+    cell: ({ row }) => (
+        <div>
+      {new Intl.DateTimeFormat('en-US', { day: 'numeric', month: 'short', year: 'numeric' }).format(new Date(row.getValue('startDate')))}
+    </div>
+      ),
   },
   {
     accessorKey: 'price',
     header: 'Price',
     cell: ({ row }) => (
       <div>
-        {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(row.getValue('price'))}
+        {new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(row.getValue('price'))}
       </div>
     ),
   },
@@ -51,16 +60,25 @@ export const programColumns: ColumnDef<Program>[] = [
 
 const ProgramsPage: React.FC = () => {
   const [programData, setProgramData] = useState<Program[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<any>(null);
 
   useEffect(() => {
     // Fetch data from the API endpoint
     const fetchProgramData = async () => {
       try {
         const response = await fetch('http://localhost:3000/api/v1/programs');
+        if (!response.ok) {
+          throw new Error(`Failed to fetch program data. Status: ${response.status}`);
+        }
+
         const data = await response.json();
         setProgramData(data.programs);
       } catch (error) {
         console.error('Error fetching program data:', error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -70,7 +88,8 @@ const ProgramsPage: React.FC = () => {
   return (
     <div>
       <h1>This is programs Page</h1>
-      <ProgramsTable data={programData} />
+      <Programs/>
+      {/* <ProgramsTable data={programData} /> */}
     </div>
   );
 };
---
+++ components/ui/crud-programs.tsx
@@ -0,0 +1,153 @@
+// pages/programs/index.tsx
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Dialog, Transition } from '@radix-ui/react-dialog';
+import { useForm } from 'react-hook-form';
+import { AiOutlineDelete, AiOutlineEdit, AiOutlinePlus } from 'react-icons/ai';
+import { format } from 'date-fns';
+
+const Programs = () => {
+  const [programs, setPrograms] = useState([]);
+  const [selectedProgram, setSelectedProgram] = useState(null);
+  const { register, handleSubmit, reset } = useForm();
+  const [open, setOpen] = useState(false);
+
+  const fetchPrograms = async () => {
+    try {
+      const result = await axios.get('http://localhost:3000/api/v1/programs');
+      setPrograms(result.data.programs);
+    } catch (error) {
+      console.error('Error fetching programs:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPrograms();
+  }, []);
+
+  const onSubmit = async (data) => {
+    try {
+       // Formatting the price value as float
+      data.price = parseFloat(data.price);
+      // Formatting the startDate value as ISO-8601 DateTime
+      data.startDate = format(new Date(data.startDate), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx");
+      // Formatting the endDate value as ISO-8601 DateTime
+      data.endDate = format(new Date(data.endDate), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx");
+
+      await axios.post('http://localhost:3000/api/v1/programs', data);
+      fetchPrograms();
+      reset();
+    } catch (error) {
+      console.error('Error adding program:', error);
+    }
+  };
+
+  const onDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/api/v1/programs/${id}`);
+      fetchPrograms();
+    } catch (error) {
+      console.error('Error deleting program:', error);
+    }
+  };
+
+  const onEdit = (program) => {
+    setSelectedProgram(program);
+    setOpen(true);
+  };
+
+  const onDialogClose = () => {
+    setSelectedProgram(null);
+    setOpen(false);
+  };
+
+  const onEditSubmit = async (data) => {
+    try {
+      await axios.patch(`http://localhost:3000/api/v1/programs/${selectedProgram.id}`, data);
+      fetchPrograms();
+      setSelectedProgram(null);
+      setOpen(false);
+    } catch (error) {
+      console.error('Error editing program:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Programs</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="mb-4">
+        <div className="flex space-x-4">
+          <div className="flex-1">
+            <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+              Title:
+            </label>
+            <input type="text" id="title" {...register('title')} className="mt-1 p-2 w-full border rounded" required />
+          </div>
+          <div className="flex-1">
+            <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+              Description:
+            </label>
+            <input
+              type="text"
+              id="description"
+              {...register('description')}
+              className="mt-1 p-2 w-full border rounded"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="flex space-x-4 mt-4">
+          <div className="flex-1">
+            <label htmlFor="startDate" className="block text-sm font-medium text-gray-700">
+              Start Date:
+            </label>
+            <input
+              type="datetime-local"
+              id="startDate"
+              {...register('startDate')}
+              className="mt-1 p-2 w-full border rounded"
+              required
+            />
+          </div>
+          <div className="flex-1">
+            <label htmlFor="endDate" className="block text-sm font-medium text-gray-700">
+              End Date:
+            </label>
+            <input
+              type="datetime-local"
+              id="endDate"
+              {...register('endDate')}
+              className="mt-1 p-2 w-full border rounded"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="flex space-x-4 mt-4">
+          <div className="flex-1">
+            <label htmlFor="price" className="block text-sm font-medium text-gray-700">
+              Price:
+            </label>
+            <input
+              type="number"
+              step="0.01"
+              id="price"
+              {...register('price')}
+              className="mt-1 p-2 w-full border rounded"
+              required
+            />
+          </div>
+        </div>
+
+        <button type="submit" className="mt-4 bg-blue-500 text-white p-2 rounded">
+          <AiOutlinePlus className="inline-block mr-2" /> Add Program
+        </button>
+      </form>
+
+      {/* Rest of the code... */}
+    </div>
+  );
+};
+
+export default Programs;
